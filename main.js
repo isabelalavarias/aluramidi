798f64ab481bc003707bb4396d7d23fb9f59e37f
isabelalavarias <126471864+isabelalavarias@users.noreply.github.com>
2023-11-14 09:04:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-alert('Olá mundo');
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento === null)
+
+}
+if (elemento && elemnto.localName === 'audio') {
+
+    elemento.play();
+}
+else {
+    //alert ('Elemento não encontrado');
+    console.log('Elemento não encontrado ou setor inválido');
+}
+}
+ 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length;) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const audio = '#som_${instrumento}'; //template strings
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+     }
+
+     tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space')
+
+        if(evento.code === 'Soace' || evento.code === 'Enter') {
+
+        }
+
+     }
+
+     tecla.onkeyup = fuction () {
+        tecla.classList.remove('ativa');
+     }
